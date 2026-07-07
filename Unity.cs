704b9bfb495687e475c981f65b3a80ca9fb0aeb6
jaisreeG <jaisreeg.ece2023@citchennai.net>
2026-07-07 21:33:08 +0530

    Create Unity.cs

---
+++ Unity.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class UnityMainThreadDispatcher : MonoBehaviour
+{
+    private static UnityMainThreadDispatcher _instance;
+    private static readonly Queue<Action> _executionQueue = new Queue<Action>();
+
+    public static UnityMainThreadDispatcher Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("UnityMainThreadDispatcher is not initialized.");
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Enqueue(Action action)
+    {
+        lock (_executionQueue)
+        {
+            _executionQueue.Enqueue(action);
+        }
+    }
+
+    void Update()
+    {
+        lock (_executionQueue)
+        {
+            while (_executionQueue.Count > 0)
+                _executionQueue.Dequeue()?.Invoke();
+        }
+    }
+}
